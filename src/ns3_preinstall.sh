393c155f0b218ad7b82d193088f3c4ddc84183bb
today2098 <ipp.10032018@gmail.com>
2022-03-02 23:32:24 +0900

    [fix] echoコマンドで表示する文字列を修正

--- src/ns3_preinstall.sh
+++ src/ns3_preinstall.sh
@@ -2,9 +2,10 @@
 
 SECONDS=0
 
+echo "The pre-install of ns3 starts."
 
 # # minimal requirements for C++ users (release 3.36 and development)
-# echo "> sudo apt -y install g++ python3 cmake"
+# echo "\n> sudo apt -y install g++ python3 cmake"
 # sudo apt -y install g++ python3 cmake
 
 # minimal requirements for Python API users (release 3.30 and newer, and ns-3-dev)
@@ -43,47 +44,47 @@ echo -e "\n> apt -y install uncrustify"
 sudo apt -y install uncrustify
 
 # # Doxygen and related inline documentation
-# echo -e "sudo apt -y install doxygen graphviz imagemagick"
+# echo -e "\n> sudo apt -y install doxygen graphviz imagemagick"
 # sudo apt -y install doxygen graphviz imagemagick
-# echo -e "sudo apt -y install texlive texlive-extra-utils texlive-latex-extra texlive-font-utils dvipng latexmk"
+# echo -e "\n> sudo apt -y install texlive texlive-extra-utils texlive-latex-extra texlive-font-utils dvipng latexmk"
 # sudo apt -y install texlive texlive-extra-utils texlive-latex-extra texlive-font-utils dvipng latexmk
 
 # # The ns-3 manual and tutorial are written in reStructuredText for Sphinx (doc/tutorial, doc/manual, doc/models), and figures typically in dia (also needs the texlive packages above)
-# echo -e "sudo apt -y install python3-sphinx dia"
+# echo -e "\n> sudo apt -y install python3-sphinx dia"
 # sudo apt -y install python3-sphinx dia
 
 # GNU Scientific Library (GSL) support for more accurate 802.11b WiFi error models (not needed for OFDM)
-echo -e "sudo apt -y install gsl-bin libgsl-dev libgslcblas0"
+echo -e "\n> sudo apt -y install gsl-bin libgsl-dev libgslcblas0"
 sudo apt -y install gsl-bin libgsl-dev libgslcblas0
 
 # To read pcap packet traces
-echo -e "sudo apt -y install tcpdump"
+echo -e "\n> sudo apt -y install tcpdump"
 sudo apt -y install tcpdump
 
 # Database support for statistics framework
-echo -e "sudo apt -y install sqlite sqlite3 libsqlite3-dev"
+echo -e "\n> sudo apt -y install sqlite sqlite3 libsqlite3-dev"
 sudo apt -y install sqlite sqlite3 libsqlite3-dev
 
 # Xml-based version of the config store (requires libxml2 >= version 2.7)
-echo -e "sudo apt -y install libxml2 libxml2-dev"
+echo -e "\n> sudo apt -y install libxml2 libxml2-dev"
 sudo apt -y install libxml2 libxml2-dev
 
 # Support for generating modified python bindings
-echo -e "sudo apt -y install cmake libc6-dev libc6-dev-i386 libclang-dev llvm-dev automake python3-pip"
+echo -e "\n> sudo apt -y install cmake libc6-dev libc6-dev-i386 libclang-dev llvm-dev automake python3-pip"
 sudo apt -y install cmake libc6-dev libc6-dev-i386 libclang-dev llvm-dev automake python3-pip
-echo -e "python3 -m pip install --user cxxfilt"
+echo -e "\n> python3 -m pip install --user cxxfilt"
 python3 -m pip install --user cxxfilt
 
 # A GTK-based configuration system
-echo -e "sudo apt -y install libgtk-3-dev"
+echo -e "\n> sudo apt -y install libgtk-3-dev"
 sudo apt -y install libgtk-3-dev
 
 # To experiment with virtual machines and ns-3
-echo -e "sudo apt -y install vtun lxc uml-utilities"
+echo -e "\n> sudo apt -y install vtun lxc uml-utilities"
 sudo apt -y install vtun lxc uml-utilities
 
 # Support for openflow module (requires libxml2-dev if not installed above) and Boost development libraries
-echo -e "sudo apt -y install libxml2 libxml2-dev libboost-all-dev"
+echo -e "\n> sudo apt -y install libxml2 libxml2-dev libboost-all-dev"
 sudo apt -y install libxml2 libxml2-dev libboost-all-dev
 
 
