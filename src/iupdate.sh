c6b44e864775788d15f044c6aaa91885488982fb
Today <ipp.30042022@gmail.com>
2022-08-12 17:45:39 +0900

    [clean] Refactoring およびメッセージの修正

--- src/iupdate.sh
+++ src/iupdate.sh
@@ -4,10 +4,12 @@ SECONDS=0
 
 echo "> sudo apt update"
 sudo apt update
+
 echo -e "\n> sudo apt -y upgrade"
 sudo apt -y upgrade
+
 echo -e "\n> sudo apt -y autoremove"
 sudo apt -y autoremove
 
 time=${SECONDS}
-echo -e "\nAll completed. This processing took ${time} seconds."
+echo -e "\nAll are completed. This process took ${time} seconds."
